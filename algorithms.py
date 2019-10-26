4248141aa4baaf65b69adee489eec3594405a89d
William Zhang <williamyixin@gmail.com>
2019-10-26 13:13:57 -0700

    Changed algorithms to be compatible with gui

--- algorithms.py
+++ algorithms.py
@@ -30,35 +30,39 @@ class Algorithms:
         def bidirectional_dijkstra(start_node, end_node):
             return []
 
-        def bellman_ford(start_node, list_edges, list_nodes):
-            #Initializing all the node labels except for the start node
-            #as having infinite distance.
-            for node in list_nodes:
-                node.label = float("Inf");
-            start_node.label = 0
+        def bellman_ford_path_gen(start_node, end_node):
 
-            #Edge relaxation algorithm
-            for edge in list_edges:
-                distance = edge.end.weight - edge.start.weight
-                if distance > 0:
-                    distance = pow(2,distance) #double if it's uphill: change to exponential?
-                if edge.end.label > edge.start.label + distance:
-                    edge.end.label = edge.start.label + distance
-                    edge.fastest_trace_previous_node = edge.start
-            #Detect negative weight cycle paradoxes
-            for edge in list_edges:
-                if edge.end.label > edge.start.label:
-                    print("Negative weight cycle detected")
+
+            def bellman_ford(start_node, list_edges, list_nodes):
+                #Initializing all the node labels except for the start node
+                #as having infinite distance.
+                for node in list_nodes:
+                    node.label = float("Inf");
+                start_node.label = 0
+
+                #Edge relaxation algorithm
+                for edge in list_edges:
+                    distance = edge.end.weight - edge.start.weight
+                    if distance > 0:
+                        distance = pow(2,distance) #double if it's uphill: change to exponential?
+                    if edge.end.label > edge.start.label + distance:
+                        edge.end.label = edge.start.label + distance
+                        edge.fastest_trace_previous_node = edge.start
+                #Detect negative weight cycle paradoxes
+                for edge in list_edges:
+                    if edge.end.label > edge.start.label:
+                        print("Negative weight cycle detected")
+
+            return self.getPath(end_node)
 
         #Gets the path. Assumes the graph has already been processed through
         #an algorithm.
-        def getPath(end_node):
-            if end_node.fastest_trace_previous_node:
-                return getPath(end_node.fastest_trace_previous_node) + [end_node]
-            return [end_node]
-
-
-
 
         self.function_map = {"Simple Dijkstra": simple_dijkstra,
-                             "Bidrectional Dijkstra": bidirectional_dijkstra}
+                             "Bidrectional Dijkstra": bidirectional_dijkstra,
+                             "Bellman Fords": bellman_ford_path_gen}
+
+    def getPath(end_node):
+        if end_node.fastest_trace_previous_node:
+            return getPath(end_node.fastest_trace_previous_node) + [end_node]
+        return [end_node]
