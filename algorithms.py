cd45584872a67cce3b563167bb9bcca8539c00a6
Connor <connorlien19@gmail.com>
2019-10-26 13:18:45 -0700

    indenting change

--- algorithms.py
+++ algorithms.py
@@ -8,24 +8,24 @@ class Algorithms:
 
         def simple_dijkstra(start_node, end_node): #only returns the dictionary of distances so far
 
-        # call "main_frame.highlight_node(node)" to highlight a node
-        main_frame.highlight_node(start_node)
+            # call "main_frame.highlight_node(node)" to highlight a node
+            main_frame.highlight_node(start_node)
 
-        # Actual Dijkstra's stuff
-        priority_queue, node_list = [(0, start_node)], Node.node_list[:]
-        distances = {i : float('inf') for i in node_list}
-        distances[start_node] = 0
-        # Finds node with the smallest distance value
-        while priority_queue:
-            current_distance, current_vertex = heapq.heappop(priority_queue)
-            if current_distance < distances[current_vertex]:
-                neighbors = [i for i in node_list if i is current_vertex][0].connections
-                for n in neighbors:
-                    distance = current_distance + n.weight
-                    if distance < distances[n]:
-                        distances[n] = distance
-                        heapq.heappush(priority_queue, (distance, n))
-        return distances
+            # Actual Dijkstra's stuff
+            priority_queue, node_list = [(0, start_node)], Node.node_list[:]
+            distances = {i : float('inf') for i in node_list}
+            distances[start_node] = 0
+            # Finds node with the smallest distance value
+            while priority_queue:
+                current_distance, current_vertex = heapq.heappop(priority_queue)
+                if current_distance < distances[current_vertex]:
+                    neighbors = [i for i in node_list if i is current_vertex][0].connections
+                    for n in neighbors:
+                        distance = current_distance + n.weight
+                        if distance < distances[n]:
+                            distances[n] = distance
+                            heapq.heappush(priority_queue, (distance, n))
+            return distances
 
         def bidirectional_dijkstra(start_node, end_node):
             return []
