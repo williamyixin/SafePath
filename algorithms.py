f552313d146e67f049edb641f76f481ac680f5e6
William Zhang <williamyixin@gmail.com>
2019-10-26 11:30:40 -0700

    Bellman-ford's algorithm mostly implemented
    Just need to trace nodes

--- algorithms.py
+++ algorithms.py
@@ -30,5 +30,25 @@ class Algorithms:
         def bidirectional_dijkstra(start_node, end_node):
             return []
 
+        def bellman_ford(start_node, list_edges, list_nodes):
+            for node in list_nodes:
+                node.label = float("Inf");
+            start_node.label = 0
+
+            for edge in list_edges:
+                distance = edge.end.weight - edge.start.weight
+                if distance > 0:
+                    distance = pow(2,distance) #double if it's uphill: change to exponential?
+                if edge.end.label > edge.start.label + distance:
+                    edge.end.label = edge.start.label + distance
+
+            for edge in list_edges:
+                if edge.end.label > edge.start.label:
+                    print("Negative weight cycle detected")
+
+
+
+
+
         self.function_map = {"Simple Dijkstra": simple_dijkstra,
                              "Bidrectional Dijkstra": bidirectional_dijkstra}
