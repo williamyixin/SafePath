e81b40a69eefee27e3704199740d8af22e40d1a1
Connor <connorlien19@gmail.com>
2019-10-26 15:02:15 -0700

    i might have a dijkstras algorithm

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from node import Node
-import heapq
+from collections import deque
 
 def __init__(self, main_frame):
     def simple_dijkstra(start_node, end_node): #only returns the dictionary of distances so far
@@ -8,17 +8,37 @@ def __init__(self, main_frame):
         main_frame.highlight_node(start_node)
 
         # Actual Dijkstra's stuff
-        priority_queue, node_list = [(0, start_node)], Node.node_list[:]
-        distances = {i : float('inf') for i in node_list}
+        node_list = Node.node_list[:]
+        distances, previous_nodes = {i : float('inf') for i in node_list}, {i : None for i in node_list}
         distances[start_node] = 0
+        nodes = node_list.copy()
+                
+        while nodes:
+            current_node = min(nodes, lambda x: distances[x])
+            if distances[current_node] == float('inf'):
+                break
+            for neighbor in current_node.connections:
+                alternative = distances[current_node] + neighbor.weight
+                if alternative < distances[neighbor]:
+                    distances[neighbor] = alternative
+                    previous_nodes[neighbor] = current_node
+        nodes.remove(current_node)
+
+        path, current_node = [], end_node
+        while previous_nodes[current_node]:
+            path.insert(0, current_node)
+            current_node = previous_nodes[current_node]
+        if path:
+            path.insert(0, current_node)
+        return path
+
         # Finds node with the smallest distance value
-        while priority_queue:
-            current_distance, current_vertex = heapq.heappop(priority_queue)
-            if current_distance < distances[current_vertex]:
-                neighbors = [i for i in node_list if i is current_vertex][0].connections
-                for n in neighbors:
-                    distance = current_distance + n.weight
-                    if distance < distances[n]:
-                        distances[n] = distance
-                        heapq.heappush(priority_queue, (distance, n))
-        return distances
+        # while priority_queue:
+        #     current_node = min([x for x,y in distances.items()])
+        #     if current_node is end_node:
+        #         return distances[current_node]
+        #     for neighbor in current_node.connections:
+        #         distance = neighbor.weight - current_node.weight
+        #         if distance < distances[neighbor]:
+        #             distances[neighbor] = distance
+        #             priority_queue += (distance, neighbor)
