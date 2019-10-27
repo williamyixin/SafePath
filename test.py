97dba421fb175884923dbf6e4d003c73f7eca8c8
Connor <connorlien19@gmail.com>
2019-10-26 18:26:15 -0700

    created a test case for determining elevation

--- test.py
+++ test.py
@@ -1,44 +1,21 @@
-from node import Node
-from collections import deque
+from __future__ import print_function
+import json
+import requests
 
-def __init__(self, main_frame):
-    def simple_dijkstra(start_node, end_node): #only returns the dictionary of distances so far
+def elevation(lat, lng):
+    apikey = "AIzaSyBKatwBAmFSYjefKc2a6QPPdvMPG57Pwbg"
+    url = "https://maps.googleapis.com/maps/api/elevation/json"
+    request = requests.get(url+"?locations="+str(lat)+","+str(lng)+"&key="+apikey)
+    try:
+        results = json.loads(request.text).get('results')
+        if 0 < len(results):
+            elevation = results[0].get('elevation')
+            #resolution = results[0].get('resolution') # for RESOLUTION
+            # ELEVATION
+            return elevation
+        else:
+            print('HTTP GET Request failed.')
+    except ValueError as e:
+        print('JSON decode failed: '+str(request) + str(e))
 
-        # call "main_frame.highlight_node(node)" to highlight a node
-        main_frame.highlight_node(start_node)
-
-        # Actual Dijkstra's stuff
-        node_list = Node.node_list[:]
-        distances, previous_nodes = {i : float('inf') for i in node_list}, {i : None for i in node_list}
-        distances[start_node] = 0
-        nodes = node_list.copy()
-                
-        while nodes:
-            current_node = min(nodes, lambda x: distances[x])
-            if distances[current_node] == float('inf'):
-                break
-            for neighbor in current_node.connections:
-                alternative = distances[current_node] + neighbor.weight
-                if alternative < distances[neighbor]:
-                    distances[neighbor] = alternative
-                    previous_nodes[neighbor] = current_node
-        nodes.remove(current_node)
-
-        path, current_node = [], end_node
-        while previous_nodes[current_node]:
-            path.insert(0, current_node)
-            current_node = previous_nodes[current_node]
-        if path:
-            path.insert(0, current_node)
-        return path
-
-        # Finds node with the smallest distance value
-        # while priority_queue:
-        #     current_node = min([x for x,y in distances.items()])
-        #     if current_node is end_node:
-        #         return distances[current_node]
-        #     for neighbor in current_node.connections:
-        #         distance = neighbor.weight - current_node.weight
-        #         if distance < distances[neighbor]:
-        #             distances[neighbor] = distance
-        #             priority_queue += (distance, neighbor)
+print(elevation(48.512315, 12.434518))
