2dc7f9cae135d3ba9ca094669173fd6dbbf821d0
Connor <connorlien19@gmail.com>
2019-10-27 00:23:25 -0700

    new data for kauai island

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import sys
 import random #for testing only
 from math import sqrt
 
-ASPECT_RATIO = 0.5*1.15470053838*23/18
+ASPECT_RATIO = 0.5*1.15470053838*69
 
 def elevation(lat, lng):
     # return random.randint(200,500)
@@ -25,8 +25,9 @@ def elevation(lat, lng):
     except ValueError as e:
         print('JSON decode failed: '+str(request) + str(e))
 
-def collectdata(start_lat, start_lon, tile_size, grid_x, grid_y): #start_lat and start_lon are the upper left (north west) coordinates
+def collectdata(start_lat, start_lon, tile_size, grid_x, grid_y, lon_offset): #start_lat and start_lon are the upper left (north west) coordinates
     #grid_x and #grid_y are how many measurements are being taken for each direction
+    #lon_offset is the number of miles one degree of longitude is at any given latitude (can be calculated with online calculator)
     f = open("topography_testing1.txt","w+")
     num_data_points = grid_x * grid_y
     counter = 0
@@ -34,7 +35,7 @@ def collectdata(start_lat, start_lon, tile_size, grid_x, grid_y): #start_lat and
     for i in range(grid_y):
         for j in range(grid_x):
             multiplier = (1-2*(i%2)) * (1-2*(j%2))
-            curr_lat, curr_lon = start_lat - tile_size * i + (multiplier * (tile_size * sqrt(3) / 6)), start_lon + tile_size * ASPECT_RATIO * j
+            curr_lat, curr_lon = start_lat - tile_size * i + (multiplier * (tile_size * sqrt(3) / 6)), start_lon + tile_size * (ASPECT_RATIO/lon_offset) * j
             elev = elevation(curr_lat, curr_lon)
 
             progress = str(round(100*counter/num_data_points))
@@ -49,4 +50,4 @@ def collectdata(start_lat, start_lon, tile_size, grid_x, grid_y): #start_lat and
 
 #collectdata(0, 0, 1, 50, 25)
 command_args = sys.argv
-collectdata(float(command_args[1]), float(command_args[2]), float(command_args[3]), int(command_args[4]), int(command_args[5]))
+collectdata(float(command_args[1]), float(command_args[2]), float(command_args[3]), int(command_args[4]), int(command_args[5]), int(command_args[5]))
